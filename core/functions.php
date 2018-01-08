32b0645b3004692be37041e31148af6c6202d6f2
Konstantin Polyakov <kostyan_93@mail.ru>
2018-01-09 02:29:07 +0300

    coding was changed into utf-8

--- core/functions.php
+++ core/functions.php
@@ -1,110 +1,102 @@
-<?php
-/*
- * Author: Polyakov Konstantin 
- */
-
-define('CUSTOM_FUNCTIONS_LOADED', true);
-
-function echo_creator($link_style='', $image_style='', $sign_style='') {
-        ?>
-
-    <a style='border: 1px solid #fff;padding:12px;<?=$link_style?>' target="_blank" href="http://âàøñàéò.èíôî-ðô.ðô">
-            <nobr>
-                        <img style='<?=$image_style?>' height="30px" src="https://èíôî-ðô.ðô/media/img/informer.png">
-                        <span style='<?=$sign_style?>'>&nbsp;&nbsp;Äèçàéí è ðàçðàáîòêà</span>
-                </nobr>
-        </a>
-
-    <?php
-}
-
-function echoImageLoader($name, $image_url, $w, $h, $is_ret=false) {
-   $s = '';
-   $s .=  '<div style="width:'.$w.'; height:'.$h.'; background-image:url('.$image_url.'?a='.time().'); background-size: contain; background-repeat: no-repeat; border: 2px solid #C7D8EA;">';
-   $s .= '<input style="opacity:0; cursor:pointer; width:'.$w.'; height:'.$h.';" name="'.$name.'" type="file" onchange="show_pic(this.files[0], function(image_data, el) {el.parentElement.style.backgroundImage = \'url(\'+image_data+\')\'}, this)">';
-   $s .= '</div>';
-   
-   if ($is_ret) return $s;
-   echo $s;
-}
-
-function generate_image_name($len=15, $registr='both') {
-        $salt = "0123456789";
-        if (in_array($registr, ['both', 'up'])) $salt .= "ABCHEFGHJKMNPQRSTUVWXYZ";
-        if (in_array($registr, ['both', 'low'])) $salt .= "abchefghjkmnpqrstuvwxyz";
-        $name = '';
-        srand((double)microtime()*1000000);
-        $i = 0;
-        while ($i <= $len) {
-                $num = rand() % strlen($salt);
-                $tmp = substr($salt, $num, 1);
-                $name = $name . $tmp;
-                $i++;
-        }
-        return $name;
-}
-
-// http://php.net/manual/ru/reserved.variables.files.php#109958
-function diverse_array($vector) { 
-    $result = array(); 
-    foreach($vector as $key1 => $value1) 
-        foreach($value1 as $key2 => $value2) 
-            $result[$key2][$key1] = $value2; 
-    return $result; 
-}
-
-/*
-   ïå÷àòàþò ñîîáùåíèÿ (óâåäîìèòåëüíûå èëè îøèáêè). Äëÿ ðàáîòû ôóíêöèé òðåáóåòñÿ ñîçäàòü ïóñòîé ìàññèâ $res
-*/
-function print_message($message, $options, $type) {
-        global $admin;
-        
-    if (!isset($options['return_url'])) $options['return_url'] = 'index.php';
-    if (!isset($options['data'])) $options['data'] = [];
-    $format =  isset($options['format']) ? $options['format'] : 'json';
-
-        if ($format == 'html') {
-                $admin->print_header();
-                if ($type == 'error') $admin->print_error($message, $options['return_url']);
-                else if ($type == 'success') $admin->print_success($message, $options['return_url']);
-                die;
-        } else if ($format == 'json') {
-                $res = [];
-                if ($type == 'error') $res['success'] = '0';
-                else if ($type == 'success') $res['success'] = '1';
-                $res['message'] = $message;
-                if (isset($options['data']))          $res['data'] = $options['data'];
-                if (isset($options['absent_fields'])) $res['absent_fields'] = $options['absent_fields'];
-                if (isset($options['timeout']))       $res['timeout'] = $options['timeout'];
-                if (isset($options['location']))      $res['location'] = $options['location'];
-                if (isset($options['title']))          $res['title'] = $options['title'];
-                echo json_encode($res);
-                die();
-        }
-}
-function print_error($message, $options=[]) { print_message($message, $options, 'error');
-}
-function print_success($message, $options=[]) { print_message($message, $options, 'success'); }
-
-// http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
-
-function startsWith($haystack, $needle) {
-
-    $length = strlen($needle);
-
-    return (substr($haystack, 0, $length) === $needle);
-
-}
-
-function endsWith($haystack, $needle) {
-
-   $length = strlen($needle);
-
-   if ($length == 0) { return true; }
-
-
-
-   return (substr($haystack, -$length) === $needle);
-
-}
+<?php
+/*
+ * Author: Polyakov Konstantin 
+ */
+
+define('CUSTOM_FUNCTIONS_LOADED', true);
+
+function echo_creator($link_style='', $image_style='', $sign_style='') {
+        ?>
+
+    <a style='border: 1px solid #fff;padding:12px;<?=$link_style?>' target="_blank" href="http://Ð²Ð°ÑˆÑÐ°Ð¹Ñ‚.Ð¸Ð½Ñ„Ð¾-Ñ€Ñ„.Ñ€Ñ„">
+            <nobr>
+                        <img style='<?=$image_style?>' height="30px" src="https://Ð¸Ð½Ñ„Ð¾-Ñ€Ñ„.Ñ€Ñ„/media/img/informer.png">
+                        <span style='<?=$sign_style?>'>&nbsp;&nbsp;Ð”Ð¸Ð·Ð°Ð¹Ð½ Ð¸ Ñ€Ð°Ð·Ñ€Ð°Ð±Ð¾Ñ‚ÐºÐ°</span>
+                </nobr>
+        </a>
+
+    <?php
+}
+
+function echoImageLoader($name, $image_url, $w, $h, $is_ret=false) {
+   $s = '';
+   $s .=  '<div style="width:'.$w.'; height:'.$h.'; background-image:url('.$image_url.'?a='.time().'); background-size: contain; background-repeat: no-repeat; border: 2px solid #C7D8EA;">';
+   $s .= '<input style="opacity:0; cursor:pointer; width:'.$w.'; height:'.$h.';" name="'.$name.'" type="file" onchange="show_pic(this.files[0], function(image_data, el) {el.parentElement.style.backgroundImage = \'url(\'+image_data+\')\'}, this)">';
+   $s .= '</div>';
+ 
+   if ($is_ret) return $s;
+   echo $s;
+}
+
+function generate_image_name($len=15, $registr='both') {
+        $salt = "0123456789";
+        if (in_array($registr, ['both', 'up'])) $salt .= "ABCHEFGHJKMNPQRSTUVWXYZ";
+        if (in_array($registr, ['both', 'low'])) $salt .= "abchefghjkmnpqrstuvwxyz";
+        $name = '';
+        srand((double)microtime()*1000000);
+        $i = 0;
+        while ($i <= $len) {
+                $num = rand() % strlen($salt);
+                $tmp = substr($salt, $num, 1);
+                $name = $name . $tmp;
+                $i++;
+        }
+        return $name;
+
+}
+
+// http://php.net/manual/ru/reserved.variables.files.php#109958
+function diverse_array($vector) { 
+    $result = array(); 
+    foreach($vector as $key1 => $value1) 
+        foreach($value1 as $key2 => $value2) 
+            $result[$key2][$key1] = $value2; 
+    return $result; 
+}
+
+
+/*
+   Ð¿ÐµÑ‡Ð°Ñ‚Ð°ÑŽÑ‚ ÑÐ¾Ð¾Ð±Ñ‰ÐµÐ½Ð¸Ñ (ÑƒÐ²ÐµÐ´Ð¾Ð¼Ð¸Ñ‚ÐµÐ»ÑŒÐ½Ñ‹Ðµ Ð¸Ð»Ð¸ Ð¾ÑˆÐ¸Ð±ÐºÐ¸). Ð”Ð»Ñ Ñ€Ð°Ð±Ð¾Ñ‚Ñ‹ Ñ„ÑƒÐ½ÐºÑ†Ð¸Ð¹ Ñ‚Ñ€ÐµÐ±ÑƒÐµÑ‚ÑÑ ÑÐ¾Ð·Ð´Ð°Ñ‚ÑŒ Ð¿ÑƒÑÑ‚Ð¾Ð¹ Ð¼Ð°ÑÑÐ¸Ð² $res
+*/
+function print_message($message, $options, $type) {
+        global $admin;
+    if (!isset($options['return_url'])) $options['return_url'] = 'index.php';
+    if (!isset($options['data'])) $options['data'] = [];
+    $format =  isset($options['format']) ? $options['format'] : 'json';
+        if ($format == 'html') {
+                $admin->print_header();
+                if ($type == 'error') $admin->print_error($message, $options['return_url']);
+                else if ($type == 'success') $admin->print_success($message, $options['return_url']);
+                die;
+        } else if ($format == 'json') {
+                $res = [];
+                if ($type == 'error') $res['success'] = '0';
+                else if ($type == 'success') $res['success'] = '1';
+                $res['message'] = $message;
+                if (isset($options['data']))          $res['data'] = $options['data'];
+                if (isset($options['absent_fields'])) $res['absent_fields'] = $options['absent_fields'];
+                if (isset($options['timeout']))       $res['timeout'] = $options['timeout'];
+                if (isset($options['location']))      $res['location'] = $options['location'];
+                if (isset($options['title']))          $res['title'] = $options['title'];
+                echo json_encode($res);
+                die();
+        }
+}
+function print_error($message, $options=[]) { print_message($message, $options, 'error');
+}
+function print_success($message, $options=[]) { print_message($message, $options, 'success'); }
+
+// http://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+
+function startsWith($haystack, $needle) {
+    $length = strlen($needle);
+    return (substr($haystack, 0, $length) === $needle);
+}
+
+function endsWith($haystack, $needle) {
+   $length = strlen($needle);
+   if ($length == 0) { return true; }
+   return (substr($haystack, -$length) === $needle);
+}
+
 ?>

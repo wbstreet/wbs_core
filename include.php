c485461dc3f02402e371350c8b93f46762522fb1
Konstantin Polyakov <kostyan_93@mail.ru>
2018-01-08 17:05:34 +0300

    you can include files from "core_client" or "include_client"

--- include.php
+++ include.php
@@ -4,21 +4,26 @@ if(defined('WB_PATH') == false) { die("Cannot access this file directly"); }
 
 if (!function_exists('wbs_core_include')) {
     // for including js and css
-    function wbs_core_include($file_names) {
-            foreach ($file_names as $i => $file_name) {
+    function wbs_core_include($file_names, $is_vendor=false) {
+        
+        if ($is_vendor) $source_path = 'include_client';
+        else $source_path = 'core_client';
+        
+        foreach ($file_names as $i => $file_name) {
     
-                    $const = strtoupper(str_replace('.', '_', $file_name));
-                    if (defined($const)) continue;
-                    else define($const, true);
-    
-                    $extension = pathinfo($file_name)['extension'];
-                    $link = WB_URL.'/modules/wbs_core/core_client/'.$file_name;
-                    if ($extension == 'css') {
-                            echo "<link href='$link' rel='stylesheet'>";
-                    } else if ($extension == 'js') {
-                            echo "<script src='$link'></script>";
-                    }
+            $const = strtoupper(str_replace('.', '_', $file_name));
+            $const = strtoupper(str_replace('/', '_', $file_name));
+            if (defined($const)) continue;
+            else define($const, true);                    
+
+            $extension = pathinfo($file_name)['extension'];
+            $link = WB_URL."/modules/wbs_core/$source_path/$file_name";
+            if ($extension == 'css') {
+                echo "<link href='$link' rel='stylesheet' type='text/css'>";
+            } else if ($extension == 'js') {
+                echo "<script src='$link'></script>";
             }
+       }
     }
 }
 ?>

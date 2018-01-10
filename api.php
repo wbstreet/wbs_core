7f1e4db60b90221d0abc1cb88420492ed9641026
Konstantin Polyakov <kostyan_93@mail.ru>
2018-01-10 19:16:37 +0300

    api for feedback was added

--- api.php
+++ api.php
@@ -12,6 +12,53 @@ if ($action=='get_agreement') {
         if ($clsAgreemnt->is_error()) $text = $clsAgreemnt->get_error();
         print_success($text, ['title'=>'Пользовательское соглашение']);
 
+} else if ($action=='send_feedback') {
+
+    // проверка капчи
+    if (!isset($_SESSION['captcha']) || !isset($_POST['captcha']) || (string)$_SESSION['captcha'] != (string)$_POST['captcha']) {
+        print_error("Капча введена неверно! Повторите попытку. ");
+    }
+
+    // проверка согласия пользователя
+    if (!isset($clsAgreemnt) || !$clsAgreemnt->does_visitor_agree()) {
+        print_error("Вам необходимо согласиться с соглашением! ");
+    }
+    
+    // Определяем сайт
+    if (file_exists(__DIR__.'/include/idna_convert/idna_convert.class.php')) require_once(__DIR__.'/include/idna_convert/idna_convert.class.php');
+    if (class_exists('idna_convert')) {
+        $IDN = new idna_convert();
+        $url = $IDN->decode(WB_URL);
+    } else { $url = WB_URL; }
+
+    if (defined('CUSTOMSETTINGS_FEEDBACK_EMAIL') && trim(CUSTOMSETTINGS_FEEDBACK_EMAIL) != '') {
+        $email = CUSTOMSETTINGS_FEEDBACK_EMAIL;
+    } else {
+        $email = "wbstreet@mail.ru";
+    }
+
+    $arrFields = [
+        'fio'=>'ФИО',
+        'phone'=>'Телефон',
+        'zayavka'=>'Текст',
+    ];
+
+    // склеивание полей
+    $strFields = '';
+    foreach($arrFields as $lat_name => $rus_name) {
+        $strFields .= "\n\n{$rus_name}: {$_POST[$lat_name]}";
+    }
+
+    $r = $clsEmail->send(
+        $email,
+        "Письмо с Вашего сайта: \n{$strFields}",
+        "Письмо с Вашего сайта ".$url,
+        0, false
+    );
+    if ($r[0] !== true) print_error('Письмо не отправлено! ');
+
+    print_success('Письмо успешно отправлено!');
+
 } else {print_error('неверный api name');}
 
 ?>

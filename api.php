d82c9e24c247ca46701a1fa4114abe32b03af2bd
Konstantin Polyakov <kostyan_93@mail.ru>
2018-01-18 00:57:44 +0300

    feedback sending is working now.

--- api.php
+++ api.php
@@ -13,17 +13,31 @@ if ($action=='get_agreement') {
         print_success($text, ['title'=>'Пользовательское соглашение']);
 
 } else if ($action=='send_feedback') {
+    
+    //print_error(json_encode($_SESSION['captcha']).' - '.json_encode($_POST['captcha']));
+
+    $arrFields = [
+        'fio'=>'ФИО',
+        'phone'=>'Телефон',
+        'zayavka'=>'Текст',
+    ];
+    
+    foreach($arrFields as $lat_name => $rus_name) {
+        $clsFilter->f($lat_name, [['1', "Введите $rus_name!"]], 'append');
+    }
+    $clsFilter->f('captcha', [['1', "Введите Защитный код!"]], 'append');
+    if ($clsFilter->is_error()) $clsFilter->print_error();
+
+    // проверка согласия пользователя
+    if (!isset($clsAgreemnt) || !$clsAgreemnt->does_visitor_agree()) {
+        print_error("Вам необходимо согласиться с соглашением! ");
+    }
 
     // проверка капчи
     if (!isset($_SESSION['captcha']) || !isset($_POST['captcha']) || (string)$_SESSION['captcha'] != (string)$_POST['captcha']) {
         print_error("Капча введена неверно! Повторите попытку. ");
     }
 
-    // проверка согласия пользователя
-    if (!isset($clsAgreemnt) || !$clsAgreemnt->does_visitor_agree()) {
-        print_error("Вам необходимо согласиться с соглашением! ");
-    }
-    
     // Определяем сайт
     if (file_exists(__DIR__.'/include/idna_convert/idna_convert.class.php')) require_once(__DIR__.'/include/idna_convert/idna_convert.class.php');
     if (class_exists('idna_convert')) {
@@ -36,12 +50,6 @@ if ($action=='get_agreement') {
     } else {
         $email = "wbstreet@mail.ru";
     }
-
-    $arrFields = [
-        'fio'=>'ФИО',
-        'phone'=>'Телефон',
-        'zayavka'=>'Текст',
-    ];
 
     // склеивание полей
     $strFields = '';

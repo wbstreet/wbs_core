018a4ff914d52bc6769b2c6db0acb0e55a4cd8e1
Konstantin Polyakov <kostyan_93@mail.ru>
2018-02-05 16:40:05 +0300

    New functions and vars were added

--- core/class.wb_module.php
+++ core/class.wb_module.php
@@ -12,19 +12,40 @@ class Addon {
         $this->urlMedia = WB_URL.MEDIA_DIRECTORY."/mod_{$this->name}/";
         $this->pathMedia = WB_PATH.MEDIA_DIRECTORY."/mod_{$this->name}/";
         $this->pathTemplates = WB_PATH."/modules/{$this->name}/templates/";
+        $this->urlAPI = WB_URL."/modules/{$this->name}/api.php";
         if (! is_dir($this->pathMedia)) {mkdir($this->pathMedia, 0777, true);}
     }
-
+    
     function getUrlAction($action) {
         return WB_URL."/modules/{$this->name}/save.php?action={$action}&section_id={$this->section_id}&page_id={$this->page_id}";
     }
+   
+    function print_error($message, $options=[]) {
 
-    function print_error($message, $options=[]) {
-        if (!isset($options['format'])) $options['format'] = 'js';
-        $message = "Модуль: {$this->name}; page_id: {$this->page_id}; section_id: {$this->section_id}\n".$message;
-        print_error(htmlentities($message, ENT_QUOTES), $options);
+        if (!isset($options['format'])) $options['format'] = 'js';
+
+        $message = "Модуль: {$this->name}; page_id: {$this->page_id}; section_id: {$this->section_id}\n".$message;
+
+        print_error(htmlentities($message, ENT_QUOTES), $options);
     }
     
+    function install() {
+    }
+
+    function uninstall() {
+    }
+    
+    function _import_sql($filepath, $name) {
+        // create tables from sql dump file
+        $dirpath = dirname($filepath);
+        $filename = $dirpath.'/install-'.$name.'.sql';
+        if (file_exists($filename) && is_readable($filename)) {
+        $r = $database->SqlImport($filename, TABLE_PREFIX, $filepath);
+        if ($database->is_error()) {
+            return $database->get_error();
+        }
+        return true;
+    }
 }
 
 ?>

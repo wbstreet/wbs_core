0d4eec829288b82c01377b15a26d1071fba81aa6
Konstantin Polyakov <shyzik_93@mail.ru>
2018-08-09 00:38:52 +0300

    function "clipToClipboard" was added

--- core_client/functions.js
+++ core_client/functions.js
@@ -789,3 +789,16 @@ function DND(element, options) {
     element.addEventListener('mousedown', dnd); // для мыши
     element.addEventListener('touchstart', dnd, {passive:true}); // для сенсорного дисплея
 }
+
+// https://habr.com/company/ruvds/blog/358494/
+// https://stackoverflow.com/questions/22581345/click-button-copy-to-clipboard-using-jquery/30905277#30905277
+// https://ru.stackoverflow.com/questions/555709/jquery-%D0%9A%D0%B0%D0%BA-%D1%81%D0%BA%D0%BE%D0%BF%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D1%82%D1%8C-%D1%82%D0%B5%D0%BA%D1%81%D1%82-%D0%B8%D0%B7-div-%D0%B2-%D0%B1%D1%83%D1%84%D0%B5%D1%80-%D0%BE%D0%B1%D0%BC%D0%B5%D0%BD%D0%B0?noredirect=1&lq=1
+function copyToClipboard(text) {
+    var element = document.createElement("div");
+    element.textContent = text;
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
